bb4d48d68614eb59ac0aa5658812beb361f656ce
Gregory Bonassina <bonassina3@gmail.com>
2022-05-31 22:14:42 -0300

    Implementado funcionalidades do react-query

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -4,9 +4,13 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { useMutation } from 'react-query';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 
 type CreateUserFormData = {
@@ -26,17 +30,29 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        });
+
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users');
+        }
+    })
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     });
 
-    const router = useRouter();
-
-    const { errors } = formState;
-
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        console.log(values);
+        await createUser.mutateAsync(values);
 
         router.push('/users');
     }
@@ -67,14 +83,14 @@ export default function CreateUser() {
                             <Input
                                 name="name"
                                 label="Nome completo"
-                                error={errors.name}
+                                error={formState.errors.name}
                                 {...register('name')}
                             />
                             <Input
                                 name="email"
                                 type="email"
                                 label="E-mail"
-                                error={errors.email}
+                                error={formState.errors.email}
                                 {...register('email')}
                             />
                         </SimpleGrid>
@@ -84,14 +100,14 @@ export default function CreateUser() {
                                 name="password"
                                 type="password"
                                 label="Senha"
-                                error={errors.password}
+                                error={formState.errors.password}
                                 {...register('password')}
                             />
                             <Input
                                 name="password_confirmation"
                                 type="password"
                                 label="Confirmação da senha"
-                                error={errors.password_confirmation}
+                                error={formState.errors.password_confirmation}
                                 {...register('password_confirmation')}
                             />
                         </SimpleGrid>
